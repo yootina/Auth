6be60589370dc3c1d6608e56f1f7184eaa17bc40
yootina <tinayoo1004@gmail.com>
2023-11-07 11:47:42 +0900

    bootstrap 적용

---
+++ accounts/forms.py
@@ -0,0 +1,9 @@
+from .models import User
+from django.contrib.auth.forms import UserCreationForm
+
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = User
+        fields = UserCreationForm.Meta.fields
+
