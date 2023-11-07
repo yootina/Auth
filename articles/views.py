30f84d93cbec115cd4ab831e05a5fd5cd69453ed
yootina <tinayoo1004@gmail.com>
2023-11-07 15:22:58 +0900

    user create 생성

--- articles/views.py
+++ articles/views.py
@@ -1,6 +1,22 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .forms import ArticleForm
 # Create your views here.
 
 def index(request):
     return render(request, 'index.html')
+
+def create(request):
+    if request.method == 'POST':
+        form = ArticleForm(request.POST)
+        
+        if form.is_valid():
+            article = form.save(commit=False)
+            article.user = request.user
+            article.save()
+            return redirect('articles:index')
+    else:
+        form = ArticleForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'form.html', context)
